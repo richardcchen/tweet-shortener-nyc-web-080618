e6ac61f9eb455bf03766779c28aa5d7b3eb3746f
Richard Chen <richard.c.chen@gmail.com>
2018-07-30 16:49:47 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,39 @@
 # Write your code here.
+def dictionary
+  dic = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&",
+  }
+end
+
+
+def word_substituter(tweet)
+  tweetarray = tweet.split(" ")
+  dictionaryorig = dictionary.keys
+  newarray = []
+  tweetarray.each_with_index do |word, index|
+    counter = 0
+    while counter < dictionaryorig.length
+      if dictionaryorig[counter] == word
+        tweetarray[index] = dictionary[dictionaryorig[counter]]
+      end
+      counter+= 1
+    end
+    puts tweetarray
+  end
+  tweetarray.join(" ")
+end
+
+def bulk_tweet_shortener(array)
+  array.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end 
