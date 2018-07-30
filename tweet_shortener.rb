373181d7b77ead181a0349f5a622560c54e93b04
Richard Chen <richard.c.chen@gmail.com>
2018-07-30 17:18:27 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,6 +33,7 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener(array)
-  string = array.join(" ")
-  puts string
+  array.each do |tweet|
+    puts word_substituter(tweet)
+  end
 end 
